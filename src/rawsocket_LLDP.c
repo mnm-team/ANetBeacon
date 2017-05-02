4a70d2cd8572159945ca03073bb4feae70771524
Dominik Bitzer <dominik.bitzer@mailbox.org>
2017-05-02 02:35:23 +0200

    verbesserte zusammengeführte Rawsocket-Datei

--- src/rawsocket_LLDP.c
+++ src/rawsocket_LLDP.c
@@ -338,38 +338,40 @@ puts("neuTest");
 					puts("problem with verification");
 				}
 				
-				break;
-			}
-		}
-	}
+				if (0 == challengeSentBool++) {
+					// delete received packet, send challenge and flush buffer
+					memset (my_received_lldp_packet->lldpReceivedPayload, 0, LLDP_BUF_SIZ);
 	
-	if (0 == challengeSentBool++) {
-		// delete received packet, send challenge and flush buffer
-		memset (my_received_lldp_packet->lldpReceivedPayload, 0, LLDP_BUF_SIZ);
+					sendChallenge (eh->ether_shost, 65534);
 	
-		sendChallenge (eh->ether_shost, 65534);
-	
-	//	SET_SELECT_FDS
-		tv.tv_sec = 0;
-		while (1) {
-			SET_SELECT_FDS
-			rv = select(maxSockFd, &readfds, NULL, NULL, &tv);
-	puts("neuneuneu");//sleep(1);
-			if (rv == -1) perror("select"); // error occurred in select()
-			else if (rv == 0) {
-				printf("Timeout occurred! All data flushed.\n");
-				break;
-			}
-			else {
-				for (int j = 0; j < numInterfaces; j++) {
-					if (FD_ISSET(sockfd[j], &readfds)) {
-	puts("Deleted something");
-						recvfrom(sockfd[j], NULL, 1500, 0, NULL, NULL);
+				//	SET_SELECT_FDS
+					tv.tv_sec = 0;
+					while (1) {
+						FD_ZERO(&readfds); for (int i = 0; i < numInterfaces; i++) {FD_SET(sockfd[i], &readfds);}
+						rv = select(maxSockFd, &readfds, NULL, NULL, &tv);
+				puts("neuneuneu");//sleep(1);
+						if (rv == -1) perror("select"); // error occurred in select()
+						else if (rv == 0) {
+							printf("Timeout occurred! All data flushed.\n");
+							break;
+						}
+						else {
+							for (int j = 0; j < numInterfaces; j++) {
+								if (FD_ISSET(sockfd[j], &readfds)) {
+				puts("Deleted something");
+									recvfrom(sockfd[j], NULL, 1500, 0, NULL, NULL);
+								}
+							}
+						}
 					}
+					break;
 				}
+				
+				break;
 			}
 		}
 	}
+	
 	
 	
 	
