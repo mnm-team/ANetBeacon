80bc73e53c8433d2851eb0e9c226040e2c7697eb
Dominik Bitzer <dominik.bitzer@mailbox.org>
2017-05-16 23:35:00 +0200

    LANbeacon enterprise switch test

--- src/main.c
+++ src/main.c
@@ -16,6 +16,7 @@
 clear && make && clear && ./lanbeacon -4 "Gobi: 192.168.178.133/24 Arktis: 111.222.111.222/16 Kalahari: 222.111.222.111/17" -6 "LRZ: 2001:618:11:1:1::1/127 MNM: 2001:cdba:0:0:0:0:3257:9652/33" -e "dominik.bitzer@mailbox.org" -d "DHCP info" -r "MAC: 00:04:4b:01:70:aa" -i 937 -n "MNM-VLAN Team IPsec" -c 'Das ist ein Beispiel fuer einen benutzerdefinierten String. Es kann beliebiger Text mitgegeben werden' && echo && xxd  -c 16 testNewTransfer
 
 - saving TCPdump: tcpdump -s 65535 -w meindump ether proto 0x88cc
+- 0x88B6
 */
 
 int main(int argc, char **argv) {
